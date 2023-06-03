fdbbc026c2b9534b20b42dda041d96bdbd165a91
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-06-03 14:04:17 +0200

    login page et son JS permettant de se connecter au site disponible

--- FrontEnd/login_page.js
+++ FrontEnd/login_page.js
@@ -3,8 +3,7 @@ const submitButton = document.querySelector("form");
 // sophie.bluel@test.tld
 // S0phie
 
-submitButton.addEventListener("submit", async function (e) {
-  e.preventDefault();
+submitButton.addEventListener("submit", async (e) => {
   const form = e.currentTarget;
   const data = new FormData(form);
   const email = data.get("email");
@@ -14,7 +13,6 @@ submitButton.addEventListener("submit", async function (e) {
     email: email,
     password: password,
   };
-
   let response = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
@@ -23,11 +21,17 @@ submitButton.addEventListener("submit", async function (e) {
     body: JSON.stringify(user),
   });
 
+  const responseBody = await response.json();
+  const token = responseBody.token;
+  const id = responseBody.userId;
   const responseHtml = response.status;
 
   if (responseHtml != 200) {
     alert("Error " + responseHtml + " : Email ou Mot de passe Incorrect");
   } else {
+    console.log(responseHtml);
+    window.sessionStorage.setItem("token", token);
+    window.sessionStorage.setItem("id", id);
     window.location.replace("./homepage_edit.html");
   }
 });
