2c389d9b78607c9cee9e06032c415f3ed11a7a1d
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-06-07 09:56:53 +0200

    Ajout des icones sur les photos pour la page édition

--- FrontEnd/functions/modal_galery.js
+++ FrontEnd/functions/modal_galery.js
@@ -12,6 +12,33 @@ export function generationCurrentPhotoGallery() {
   allFigationOfPicturesInEditionGallery.forEach(
     (figaption) => (figaption.innerHTML = "éditer")
   );
+
+  function generationIcones(figure) {
+    const currentFigure = figure;
+    const creationIconeEditPics = document.createElement("div");
+    const trashDiv = document.createElement("div");
+    const multiCrossDiv = document.createElement("div");
+    const creationTrashIcon = document.createElement("i");
+    const creationMultiCross = document.createElement("i");
+
+    creationIconeEditPics.classList = "js-edit-icones";
+    trashDiv.classList = "js-trash-div";
+    multiCrossDiv.classList = "js-multicross-div";
+    creationTrashIcon.classList = "fa-solid fa-trash-can";
+    creationMultiCross.classList = "fa-solid fa-arrows-up-down-left-right";
+
+    trashDiv.appendChild(creationTrashIcon);
+    multiCrossDiv.appendChild(creationMultiCross);
+    creationIconeEditPics.appendChild(trashDiv);
+    creationIconeEditPics.appendChild(multiCrossDiv);
+
+    currentFigure.appendChild(creationIconeEditPics);
+    // function pour ajouter les icones à chaque photo de la galerie
+  }
+
+  const figuresSelected = document.querySelectorAll("#galery-modal figure");
+
+  figuresSelected.forEach((figure) => generationIcones(figure));
 }
 
 export function cleanPhotoGallery() {
